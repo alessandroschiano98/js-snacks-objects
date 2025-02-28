70c02d0d84645e08106157b40e615f9e02aeccaf
alessandroschiano98 <schianoski98@gmail.com>
2025-02-28 11:46:21 +0100

    ARRAY DI 10 AUTOMOBILI: CREATO

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,23 +1,27 @@
-// SNACK #1
+// SNACK #1 //
 
-// CREA UN ARRAY COMPOSTO DA 10 AUTOMOBILI, ogni modello dovrà avere: marca, modello, alimentazione (benzina, diesel, elettrico, gpl, a metano //
+// CREA UN ARRAY COMPOSTO DA 10 AUTOMOBILI, ogni modello dovrà avere: marca, modello, alimentazione (benzina, diesel, elettrico, gpl, a metano) //
 
 // DIVIDI LE AUTO IN 3 ARRAYS SEPARATI: nel 1° metti solo le auto a benzina, nel 2° solo le auto a diesel, nel terzo tutto il resto delle auto //
 
 // INFINE STAMPA TUTTI E 3 ARRAY //
 
 const cars = [
-    { car1: "Mercedes", modello: "GLK", alimentazione: "diesel"  },
-    { car2: "Bmw", modello: "Serie-1", alimentazione: "diesel" },
-    { car3: "Volvo", modello: "XC-60" alimentazione: "benzina" },
-    { car4: "Audi", modello: "Q3", alimentazione: "benzina" },
-    { car5: "Fiat", modello: "Panda", alimentazione: "gpl" },
-    { car6: "Toyota", modello: "Yaris", alimentazione: "elettrica" },
-    { car7: "Suzuki", modello: "Alto", alimentazione: "elettrica" },
-    { car8: "Ford", modello: "Fiesta", alimentazione: "metano" },
-    { car9: "Tesla", modello: "Model-Y", alimentazione: "elettrica" },
-    { car10: "Citroen", modello: "C3", alimentazione: "benzina" },
-    ];
+    { marca: "Mercedes", modello: "GLK", alimentazione: "Diesel"  },
+    { marca: "Bmw", modello: "Serie-1", alimentazione: "Diesel" },
+    { marca: "Volvo", modello: "XC-60", alimentazione: "Benzina" },
+    { marca: "Audi", modello: "Q3", alimentazione: "Benzina" },
+    { marca: "Fiat", modello: "Panda", alimentazione: "Gpl" },
+    { marca: "Toyota", modello: "Yaris", alimentazione: "Elettrica" },
+    { marca: "Suzuki", modello: "Alto", alimentazione: "Elettrica" },
+    { marca: "Ford", modello: "Fiesta", alimentazione: "Metano" },
+    { marca: "Tesla", modello: "Model-Y", alimentazione: "Elettrica" },
+    { marca: "Citroen", modello: "C3", alimentazione: "Benzina" }];
+
+    console.log(cars[0].marca); 
+
+   /*  const carsBenzina = [];
+    const carsDiesel = [];
+    const carsRestanti = []; */
 
 
-    
