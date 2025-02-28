3060a2a4c077bc9d28e8d5a39c7787ae25d3c73b
alessandroschiano98 <schianoski98@gmail.com>
2025-02-28 20:06:12 +0100

    CREAZIONE ARRAY SNACK#3

---
+++ snack-4/js/script.js
@@ -0,0 +1,21 @@
+/* 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Partendo da questo, crea quindi un nuovo array inserendo, per ogni
+persona, una frase (stringa) con il nome e cognome e l’indicazione se può
+guidare, in base all’età.
+
+
+*/
+
+const persone = [
+    {nome: "Alessandro", cognome: "Schiano", eta: 26},
+    {nome: "Ines", cognome: "Giardulli", eta: 23},
+    {nome: "Alessio", cognome: "Caruso", eta: 26},
+    {nome: "Alessandro", cognome: "Acerola", eta: 26},
+    {nome: "Gabriele", cognome: "Schiano", eta: 16}
+
+]
+
+console.log(persone);
+
