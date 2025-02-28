3213fea6d148503f49be54b82f54a7b1ebc98060
alessandroschiano98 <schianoski98@gmail.com>
2025-02-28 11:14:43 +0100

    first commit

---
+++ snack-1/js/script.js
@@ -0,0 +1,23 @@
+// SNACK #1
+
+// CREA UN ARRAY COMPOSTO DA 10 AUTOMOBILI, ogni modello dovrà avere: marca, modello, alimentazione (benzina, diesel, elettrico, gpl, a metano //
+
+// DIVIDI LE AUTO IN 3 ARRAYS SEPARATI: nel 1° metti solo le auto a benzina, nel 2° solo le auto a diesel, nel terzo tutto il resto delle auto //
+
+// INFINE STAMPA TUTTI E 3 ARRAY //
+
+const cars = [
+    { car1: "Mercedes", modello: "GLK", alimentazione: "diesel"  },
+    { car2: "Bmw", modello: "Serie-1", alimentazione: "diesel" },
+    { car3: "Volvo", modello: "XC-60" alimentazione: "benzina" },
+    { car4: "Audi", modello: "Q3", alimentazione: "benzina" },
+    { car5: "Fiat", modello: "Panda", alimentazione: "gpl" },
+    { car6: "Toyota", modello: "Yaris", alimentazione: "elettrica" },
+    { car7: "Suzuki", modello: "Alto", alimentazione: "elettrica" },
+    { car8: "Ford", modello: "Fiesta", alimentazione: "metano" },
+    { car9: "Tesla", modello: "Model-Y", alimentazione: "elettrica" },
+    { car10: "Citroen", modello: "C3", alimentazione: "benzina" },
+    ];
+
+
+    
