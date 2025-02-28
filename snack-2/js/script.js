e4d6fa9a5fd30a4ee58ae31e6a66c15b964c59b6
alessandroschiano98 <schianoski98@gmail.com>
2025-02-28 12:38:19 +0100

    first commit CREAZIONE SNACK-2

---
+++ snack-2/js/script.js
@@ -0,0 +1,14 @@
+/*
+
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+Crea un nuovo array con la lista dei mammiferi.
+[ 
+  { nome: ‘leone’, famiglia: ‘felidi’, classe: ‘mammiferi’ }, 
+  { nome: ‘cane’, famiglia: ‘canidi’, classe: ‘mammiferi’ }, 
+  { nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ }, 
+]
+
+*/
+
